60065d7e8bfc506f564ae3dce066c1e4f31f1cc8
Maks <maxsouldrake@gmail.com>
2026-07-02 17:10:55 +0200

    initial commit

---
+++ src/test/kotlin/io/github/mksfilmoteka/media/FilmotekaMediaApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.mksfilmoteka.media
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FilmotekaMediaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
