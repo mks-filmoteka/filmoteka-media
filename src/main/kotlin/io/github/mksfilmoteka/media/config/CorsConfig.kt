4a20f1295065e76cc0f145d80070edfa8dada968
Maks <maxsouldrake@gmail.com>
2026-07-06 15:03:29 +0200

    add cors config

---
+++ src/main/kotlin/io/github/mksfilmoteka/media/config/CorsConfig.kt
@@ -0,0 +1,16 @@
+package io.github.mksfilmoteka.media.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class CorsConfig : WebMvcConfigurer {
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/api/**")
+            .allowedOrigins("http://localhost:5173")
+            .allowedMethods("GET", "POST", "DELETE")
+            .allowedHeaders("*")
+    }
+}
